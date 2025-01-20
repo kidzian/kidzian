84d68f7e6fc3cda05aabacae41ae3d9a87786ec7
01akhil <bhadula245@gmail.com>
2025-01-20 18:40:15 +0530

    minor changes

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Heading from '../components/Heading'
-import { BicepsFlexed, Cpu, Earth, Gamepad2, MailCheck, Rocket, Send } from 'lucide-react';
+import { BicepsFlexed, Cpu, Earth, Gamepad2, Heart, MailCheck, Rocket, Send } from 'lucide-react';
 import { FaRocket } from "react-icons/fa";
 import { FaLightbulb } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa6";
@@ -41,7 +41,9 @@ const About = () => {
   </div>
 
   <div className="flex items-start gap-4">
-  <FaStar size={40} className="pb-5 text-[#8b5cf6]" />
+  
+
+  <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill='red'   class="lucide lucide-heart"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" /></svg>
     <p className="text-sm text-[#4b5462]">
       We are committed to continuous innovation, ensuring our students stay ahead in the ever-evolving world of technology. Join us at Kidzian, where learning technology is an exciting adventure!
     </p>
