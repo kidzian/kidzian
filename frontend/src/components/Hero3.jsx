3f0848635e67967bd118322a9c4806ca42d72222
01akhil <bhadula245@gmail.com>
2025-02-11 19:47:23 +0530

    adding responsivity

--- frontend/src/components/Hero3.jsx
+++ frontend/src/components/Hero3.jsx
@@ -214,7 +214,7 @@ const Hero3 = () => {
       </div>
 
       {/* Placeholder for other content */}
-      <div className='sm:w-full md:w-[50vw] lg:w-[50vw] xl:w-[50vw] 2xl:w-[50vw]  flex items-center justify-center mb-16 h-[60vh]'>
+      <div className='sm:w-full md:w-[50vw] lg:w-[50vw] xl:w-[50vw] 2xl:w-[50vw]  flex items-center justify-center  bg-yellow-400'>
         <img src={hero3} className='' alt="" />
       </div>
 
